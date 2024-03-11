659b8af741b6e933e297c92d853e48d485ca23bd
Michal Skvely <m.skvely@make.com>
2024-03-11 12:22:33 +0100

    wip: improve naming

--- src/global.ts
+++ src/global.ts
@@ -1,6 +1,6 @@
 // noinspection ES6ConvertVarToLetConst
 
-import * as imtProto from './index';
+import * as publicApi from './index';
 import './compatibility-extensions';
 
 declare global {
@@ -8,43 +8,43 @@ declare global {
   var IMT_PROTO_LOADED: boolean;
   var requireCommon: (name: string) => any;
 
-  var IMTAggregator: typeof imtProto.IMTAggregator;
-  var IMTAction: typeof imtProto.IMTAction;
-  var IMTGatewayAction: typeof imtProto.IMTGatewayAction;
-  var IMTGatewayResponder: typeof imtProto.IMTGatewayResponder;
-  var Warning: typeof imtProto.Warning;
-  var IMTHook: typeof imtProto.IMTHook;
-  var IMTConverger: typeof imtProto.IMTConverger;
-  var UnknownError: typeof imtProto.UnknownError;
-  var RuntimeError: typeof imtProto.RuntimeError;
-  var DataError: typeof imtProto.DataError;
-  var InconsistencyError: typeof imtProto.InconsistencyError;
-  var RateLimitError: typeof imtProto.RateLimitError;
-  var OutOfSpaceError: typeof imtProto.OutOfSpaceError;
-  var ConnectionError: typeof imtProto.ConnectionError;
-  var InvalidConfigurationError: typeof imtProto.InvalidConfigurationError;
-  var InvalidAccessTokenError: typeof imtProto.InvalidAccessTokenError;
-  var UnexpectedError: typeof imtProto.UnexpectedError;
-  var MaxResultsExceededError: typeof imtProto.MaxResultsExceededError;
-  var MaxFileSizeExceededError: typeof imtProto.MaxFileSizeExceededError;
-  var IncompleteDataError: typeof imtProto.IncompleteDataError;
-  var DuplicateDataError: typeof imtProto.DuplicateDataError;
-  var ModuleTimeoutError: typeof imtProto.ModuleTimeoutError;
-  var ScenarioTimeoutError: typeof imtProto.ScenarioTimeoutError;
-  var OperationsLimitExceededError: typeof imtProto.OperationsLimitExceededError;
-  var DataSizeLimitExceededError: typeof imtProto.DataSizeLimitExceededError;
-  var ExecutionInterruptedError: typeof imtProto.ExecutionInterruptedError;
-  var IMTRouter: typeof imtProto.IMTRouter;
-  var IMTListener: typeof imtProto.IMTListener;
-  var IMTAccount: typeof imtProto.IMTAccount;
-  var IMTOAuthAccount: typeof imtProto.IMTOAuthAccount;
-  var IMTTrigger: typeof imtProto.IMTTrigger;
-  var IMTGatewayTrigger: typeof imtProto.IMTGatewayTrigger;
-  var IMTPauser: typeof imtProto.IMTPauser;
-  var IMTBase: typeof imtProto.IMTBase;
-  var IMTRPC: typeof imtProto.IMTRPC;
-  var IMTFeeder: typeof imtProto.IMTFeeder;
-  var IMTTransformer: typeof imtProto.IMTTransformer;
+  var IMTAggregator: typeof publicApi.IMTAggregator;
+  var IMTAction: typeof publicApi.IMTAction;
+  var IMTGatewayAction: typeof publicApi.IMTGatewayAction;
+  var IMTGatewayResponder: typeof publicApi.IMTGatewayResponder;
+  var Warning: typeof publicApi.Warning;
+  var IMTHook: typeof publicApi.IMTHook;
+  var IMTConverger: typeof publicApi.IMTConverger;
+  var UnknownError: typeof publicApi.UnknownError;
+  var RuntimeError: typeof publicApi.RuntimeError;
+  var DataError: typeof publicApi.DataError;
+  var InconsistencyError: typeof publicApi.InconsistencyError;
+  var RateLimitError: typeof publicApi.RateLimitError;
+  var OutOfSpaceError: typeof publicApi.OutOfSpaceError;
+  var ConnectionError: typeof publicApi.ConnectionError;
+  var InvalidConfigurationError: typeof publicApi.InvalidConfigurationError;
+  var InvalidAccessTokenError: typeof publicApi.InvalidAccessTokenError;
+  var UnexpectedError: typeof publicApi.UnexpectedError;
+  var MaxResultsExceededError: typeof publicApi.MaxResultsExceededError;
+  var MaxFileSizeExceededError: typeof publicApi.MaxFileSizeExceededError;
+  var IncompleteDataError: typeof publicApi.IncompleteDataError;
+  var DuplicateDataError: typeof publicApi.DuplicateDataError;
+  var ModuleTimeoutError: typeof publicApi.ModuleTimeoutError;
+  var ScenarioTimeoutError: typeof publicApi.ScenarioTimeoutError;
+  var OperationsLimitExceededError: typeof publicApi.OperationsLimitExceededError;
+  var DataSizeLimitExceededError: typeof publicApi.DataSizeLimitExceededError;
+  var ExecutionInterruptedError: typeof publicApi.ExecutionInterruptedError;
+  var IMTRouter: typeof publicApi.IMTRouter;
+  var IMTListener: typeof publicApi.IMTListener;
+  var IMTAccount: typeof publicApi.IMTAccount;
+  var IMTOAuthAccount: typeof publicApi.IMTOAuthAccount;
+  var IMTTrigger: typeof publicApi.IMTTrigger;
+  var IMTGatewayTrigger: typeof publicApi.IMTGatewayTrigger;
+  var IMTPauser: typeof publicApi.IMTPauser;
+  var IMTBase: typeof publicApi.IMTBase;
+  var IMTRPC: typeof publicApi.IMTRPC;
+  var IMTFeeder: typeof publicApi.IMTFeeder;
+  var IMTTransformer: typeof publicApi.IMTTransformer;
   /* eslint-enable no-var */
 }
 
@@ -52,48 +52,48 @@ if (!global.IMT_PROTO_LOADED) {
   global.IMT_PROTO_LOADED = true;
 
   /**
-   * Require module from common collection of preloaded modules. This method is set later by Integration's core.
+   * Require a module from a common collection of preloaded modules. This method is set later by Integration's core.
    *
    * @param {String} name Module nane.
    * @returns {*}
    */
   if (!global.requireCommon) global.requireCommon = () => null;
 
-  global.IMTAggregator = imtProto.IMTAggregator;
-  global.IMTAction = imtProto.IMTAction;
-  global.IMTGatewayAction = imtProto.IMTGatewayAction;
-  global.IMTGatewayResponder = imtProto.IMTGatewayResponder;
-  global.Warning = imtProto.Warning;
-  global.IMTHook = imtProto.IMTHook;
-  global.IMTConverger = imtProto.IMTConverger;
-  global.UnknownError = imtProto.UnknownError;
-  global.RuntimeError = imtProto.RuntimeError;
-  global.DataError = imtProto.DataError;
-  global.InconsistencyError = imtProto.InconsistencyError;
-  global.RateLimitError = imtProto.RateLimitError;
-  global.OutOfSpaceError = imtProto.OutOfSpaceError;
-  global.ConnectionError = imtProto.ConnectionError;
-  global.InvalidConfigurationError = imtProto.InvalidConfigurationError;
-  global.InvalidAccessTokenError = imtProto.InvalidAccessTokenError;
-  global.UnexpectedError = imtProto.UnexpectedError;
-  global.MaxResultsExceededError = imtProto.MaxResultsExceededError;
-  global.MaxFileSizeExceededError = imtProto.MaxFileSizeExceededError;
-  global.IncompleteDataError = imtProto.IncompleteDataError;
-  global.DuplicateDataError = imtProto.DuplicateDataError;
-  global.ModuleTimeoutError = imtProto.ModuleTimeoutError;
-  global.ScenarioTimeoutError = imtProto.ScenarioTimeoutError;
-  global.OperationsLimitExceededError = imtProto.OperationsLimitExceededError;
-  global.DataSizeLimitExceededError = imtProto.DataSizeLimitExceededError;
-  global.ExecutionInterruptedError = imtProto.ExecutionInterruptedError;
-  global.IMTRouter = imtProto.IMTRouter;
-  global.IMTListener = imtProto.IMTListener;
-  global.IMTAccount = imtProto.IMTAccount;
-  global.IMTOAuthAccount = imtProto.IMTOAuthAccount;
-  global.IMTTrigger = imtProto.IMTTrigger;
-  global.IMTGatewayTrigger = imtProto.IMTGatewayTrigger;
-  global.IMTPauser = imtProto.IMTPauser;
-  global.IMTBase = imtProto.IMTBase;
-  global.IMTRPC = imtProto.IMTRPC;
-  global.IMTFeeder = imtProto.IMTFeeder;
-  global.IMTTransformer = imtProto.IMTTransformer;
+  global.IMTAggregator = publicApi.IMTAggregator;
+  global.IMTAction = publicApi.IMTAction;
+  global.IMTGatewayAction = publicApi.IMTGatewayAction;
+  global.IMTGatewayResponder = publicApi.IMTGatewayResponder;
+  global.Warning = publicApi.Warning;
+  global.IMTHook = publicApi.IMTHook;
+  global.IMTConverger = publicApi.IMTConverger;
+  global.UnknownError = publicApi.UnknownError;
+  global.RuntimeError = publicApi.RuntimeError;
+  global.DataError = publicApi.DataError;
+  global.InconsistencyError = publicApi.InconsistencyError;
+  global.RateLimitError = publicApi.RateLimitError;
+  global.OutOfSpaceError = publicApi.OutOfSpaceError;
+  global.ConnectionError = publicApi.ConnectionError;
+  global.InvalidConfigurationError = publicApi.InvalidConfigurationError;
+  global.InvalidAccessTokenError = publicApi.InvalidAccessTokenError;
+  global.UnexpectedError = publicApi.UnexpectedError;
+  global.MaxResultsExceededError = publicApi.MaxResultsExceededError;
+  global.MaxFileSizeExceededError = publicApi.MaxFileSizeExceededError;
+  global.IncompleteDataError = publicApi.IncompleteDataError;
+  global.DuplicateDataError = publicApi.DuplicateDataError;
+  global.ModuleTimeoutError = publicApi.ModuleTimeoutError;
+  global.ScenarioTimeoutError = publicApi.ScenarioTimeoutError;
+  global.OperationsLimitExceededError = publicApi.OperationsLimitExceededError;
+  global.DataSizeLimitExceededError = publicApi.DataSizeLimitExceededError;
+  global.ExecutionInterruptedError = publicApi.ExecutionInterruptedError;
+  global.IMTRouter = publicApi.IMTRouter;
+  global.IMTListener = publicApi.IMTListener;
+  global.IMTAccount = publicApi.IMTAccount;
+  global.IMTOAuthAccount = publicApi.IMTOAuthAccount;
+  global.IMTTrigger = publicApi.IMTTrigger;
+  global.IMTGatewayTrigger = publicApi.IMTGatewayTrigger;
+  global.IMTPauser = publicApi.IMTPauser;
+  global.IMTBase = publicApi.IMTBase;
+  global.IMTRPC = publicApi.IMTRPC;
+  global.IMTFeeder = publicApi.IMTFeeder;
+  global.IMTTransformer = publicApi.IMTTransformer;
 }
