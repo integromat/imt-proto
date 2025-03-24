6c892ac99a257d23a0396bf865ef141f79614ff8
Martin Vokřínek <m.vokrinek@make.com>
2025-03-24 15:22:58 +0100

    Adds metadata to Result

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,24 @@
 export type Bundle = { array?: Array<any> } & Record<string, any>;
 
+export type Link = {
+  type: 'link';
+  valueLabel: string;
+  resourceId: string;
+  resourceType: 'execution' | 'scenario';
+  additionalParams: Record<string, string>;
+};
+
+export type Metadata = {
+  version: number;
+  keyLabel: string;
+} & Link;
+
 export type DoneCallback = (err?: Error | null) => void;
 
 export type DoneWithInfoCallback = (err?: Error | null, done?: any) => void;
 
 export type DoneWithReportCallback = (err?: Error | null, report?: any[] | null) => void;
 
-export type DoneWithResultCallback = (err?: Error | null, result?: any) => void;
+export type DoneWithResultCallback = (err?: Error | null, result?: any, metadata?: Array<Metadata>) => void;
 
 export type DoneWithFormCallback = (err?: Error | null, form?: any) => void;
